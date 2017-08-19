5e2e240820d5042d5b0bcd3cae2b513887dced40
Christine Idouchi <cidouchi@gmail.com>
2017-08-19 17:51:30 -0400

    update project display

--- main.js
+++ main.js
@@ -10,13 +10,20 @@ $('.projects-link').click(function() {
 
 });
 
+$('.contact-link').click(function() {
+    $('html, body').animate({
+          scrollTop: $('.contact').offset().top
+        }, 500)
+
+});
+
 
 function waitingCursor() {
     $(".projects-link").typed({
             strings: [''], 
             typeSpeed: 100,
             callback: function () {
-                window.setTimeout(typeProjects, 2000);
+                window.setTimeout(typeProjects, 500);
             }
     });
 }
@@ -27,7 +34,7 @@ function typeProjects() {
             strings: ['Projects'], 
             typeSpeed: 20,
             callback: function () {
-                window.setTimeout(typeResume, 1000);
+                window.setTimeout(typeResume, 500);
             }
         });
 };
@@ -38,20 +45,20 @@ function typeResume() {
             strings: ['Resume'], 
             typeSpeed: 20,
             callback: function () {
-                window.setTimeout(typeOther, 1000);
+                window.setTimeout(typeOther, 500);
             }
         });
 };
 
 function typeOther() {
         $('.typed-cursor').hide();
-        $('.other-link').typed({
-            strings: ['Other Stuff'], 
+        $('.contact-link').typed({
+            strings: ['Contact'], 
             typeSpeed: 20,
             callback: function () {
                 window.setTimeout(function () {
                     $('.typed-cursor').hide()
-                    $('.icons').fadeIn(1000);
+                    $('.icons').fadeIn(500);
                 } ,1000);
             }
         });
